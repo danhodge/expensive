9af7dba1ea08cf1ef390bd465f5fb7fc721465a3
Dan Hodge <danhodge@gmail.com>
2020-11-26 22:19:46 -0500

    add parser combinators for hledger

---
+++ src/parser.ts
@@ -0,0 +1,82 @@
+import { Response, Streams, N, C, F } from '@masala/parser';
+
+export function nonNewlineWhitespace() {
+  return C.charIn(" \t").optrep();
+}
+
+export function nonNewlineWhitespace1() {
+  return C.charIn(" \t").then(C.charIn(" \t").optrep());
+}
+
+export function blankLine() {
+  return nonNewlineWhitespace().then(C.charIn("\n"));
+}
+
+export function date() {
+  return N.integer()
+    .then(C.char('-').drop())
+    .then(N.integer())
+    .then(C.char('-').drop())
+    .then(N.integer());
+}
+
+export function postingDescription() {
+  return (
+    C.charNotIn("\n; ").rep().then(C.char(" "))
+  ).rep()
+    .then(C.char(" ").rep());
+}
+
+export function description() {
+  return C.charNotIn("\n;")
+    .then(C.charNotIn("\n;").optrep());
+}
+
+export function amount() {
+  return C.char('$')
+    .then(C.char('-').opt())
+    .then(N.integer())
+    .then(C.char('.'))
+    .then(N.integer());
+}
+
+export function posting() {
+  return nonNewlineWhitespace1().drop()
+    .then(postingDescription())
+    .then(nonNewlineWhitespace().drop())
+    .then(amount())
+    .then(C.char("\n").drop());
+}
+
+export function record() {
+  return date()
+    .then(nonNewlineWhitespace1().drop())
+    .then(description())
+    .then(comment().opt())
+    .then(C.char("\n").drop())
+    .then(posting())
+    .then(posting().optrep())
+    .then(F.try(blankLine()).or(F.eos()).drop());
+}
+
+function comment() {
+  return C.char(';').drop()
+    .then(C.charNotIn("\n").optrep());
+}
+
+export function commentAndNewline() {
+  return comment().then(C.char("\n").drop());
+}
+
+export function hledger() {
+  return (
+    F.try(record())
+      .or(F.try(blankLine()).or(commentAndNewline()))
+  )
+    .optrep()
+    .then(F.eos());
+}
+
+export function parse(input: string): Response<any> {
+  return hledger().parse(Streams.ofString(input));
+}
---
+++ tests/parser.test.ts
@@ -0,0 +1,108 @@
+import { Streams } from '@masala/parser';
+import { readFileSync } from 'fs';
+import { parse, date, commentAndNewline, nonNewlineWhitespace, nonNewlineWhitespace1, posting, record, hledger, blankLine } from '../src/parser'
+
+test("date", () => {
+  let r = date().parse(Streams.ofString("2020-11-13"));
+  expect(r.isAccepted()).toEqual(true);
+  expect(r.value.value).toEqual([2020, 11, 13]);
+});
+
+test("nonNewlineWhitespace", () => {
+  let r = nonNewlineWhitespace().parse(Streams.ofString(" \t      \t   "));
+  expect(r.isAccepted()).toEqual(true);
+  expect(r.value.value).toEqual([" ", "\t", " ", " ", " ", " ", " ", " ", "\t", " ", " ", " "]);
+});
+
+test("nonNewlineWhitespace1 single whitespace character", () => {
+  let r = nonNewlineWhitespace1().parse(Streams.ofString("\t"));
+  expect(r.isAccepted()).toEqual(true);
+  expect(r.value.value).toEqual(["\t"]);
+});
+
+test("nonNewlineWhitespace1 multiple whitespace characters", () => {
+  let r = nonNewlineWhitespace1().parse(Streams.ofString("\t  \t"));
+  expect(r.isAccepted()).toEqual(true);
+  expect(r.value.value).toEqual(["\t", " ", " ", "\t"]);
+});
+
+test("nonNewlineWhitespace1 no whitespace characters", () => {
+  let r = nonNewlineWhitespace1().parse(Streams.ofString(""));
+  expect(r.isAccepted()).toEqual(false);
+});
+
+test("posting", () => {
+  let r = posting().parse(Streams.ofString("\texpenses:food:fast food       $-1.23\n"));
+  expect(r.isAccepted()).toEqual(true);
+  // expect(r.value.value).toEqual([]);
+});
+
+test("blankLine", () => {
+  let r = blankLine().parse(Streams.ofString("\n"));
+  expect(r.isAccepted()).toEqual(true);
+});
+
+test("blankLine with whitespace", () => {
+  let r = blankLine().parse(Streams.ofString("  \t    \n"));
+  expect(r.isAccepted()).toEqual(true);
+});
+
+test("commentAndNewline", () => {
+  let r = commentAndNewline().parse(Streams.ofString("; no comment\n\n"));
+  expect(r.isAccepted()).toEqual(true);
+  expect(r.offset).toEqual(13);
+});
+
+test("record", () => {
+  let r = record().parse(Streams.ofString("2020-11-17 Some store\n    expenses:food:groceries     $-1.23\n    assets:cash        $1.23\n"));
+  expect(r.isAccepted()).toEqual(true);
+})
+
+test("recordWithComment", () => {
+  let r = record().parse(Streams.ofString("2020-11-17 Some store  ; id=123 \n    expenses:food:groceries     $-1.23\n    assets:cash        $1.23\n"));
+  expect(r.isAccepted()).toEqual(true);
+})
+
+test("blanks", () => {
+  let data = "\n\n\n\n\n\n\n\n\n";
+  let r = hledger().parse(Streams.ofString(data));
+  expect(r.isAccepted()).toEqual(true);
+});
+
+test("comment & blank", () => {
+  let data = "; comment about nothing\n\n";
+  let r = hledger().parse(Streams.ofString(data));
+  expect(r.isAccepted()).toEqual(true);
+});
+
+test("posting", () => {
+  let data = "    expenses:unclassified                 $-900.00\n";
+  let r = posting().parse(Streams.ofString(data));
+  expect(r.isAccepted()).toEqual(true);
+})
+
+test("comment, blank & record", () => {
+  let data = "2020-01-02 Credit  ; id:1234\n" +
+    "    expenses:unclassified                 $-900.00\n" +
+    "    liabilities:credit cards:amex         $900.00\n";
+
+  let r = record().parse(Streams.ofString(data));
+  expect(r.isAccepted()).toEqual(true);
+});
+
+test("hledger", () => {
+  let data = "; journal file\n" +
+    "\n" +
+    "2020-10-01 big stuff ; id=123\n" +
+    "    expenses:import              $100.00\n" +
+    "    assets:cash                 $-100.00\n";
+
+  let r = hledger().parse(Streams.ofString(data));
+  expect(r.isAccepted()).toEqual(true);
+});
+
+// test("realData", () => {
+//   let data = readFileSync('test.journal').toString();
+//   let r = hledger().parse(Streams.ofString(data));
+//   expect(r.isAccepted()).toEqual(true);
+// });
