bf081e2e5976d736b0dc6b55bbc0447cce5dc228
Dan Hodge <danhodge@gmail.com>
2021-06-20 12:05:00 -0400

    added eslint

--- tests/storage.test.ts
+++ tests/storage.test.ts
@@ -2,11 +2,11 @@ import { basename, dirname, extname, resolve } from 'path';
 import { FileStorage } from '../src/server/storage';
 
 test("scan dir", async () => {
-  let storage = new FileStorage(resolve("./src"));
-  let scanner = (path: string) => {
+  const storage = new FileStorage(resolve("./src"));
+  const scanner = (path: string) => {
     return basename(dirname(path)) === "client" && extname(path) === ".js";
   };
-  let paths = await storage.scan(scanner);
+  const paths = await storage.scan(scanner);
 
   expect(paths.length).toEqual(1);
   expect(paths.map((p) => basename(p))).toEqual(["index.js"]);
