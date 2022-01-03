67d9141b6bff162b86ee087bb648a57b8cfd7131
danhodge <danhodge@gmail.com>
2022-01-03 10:08:26 -0500

    added test for posting rules

--- src/server/postingRules.ts
+++ src/server/postingRules.ts
@@ -11,8 +11,8 @@ export class PostingRules {
     readonly mappings: Map<string, Array<string>>,
     readonly accountName: string,
     readonly accountType: AccountType,
-    readonly defaultBalanceAccountName: string
-    ) {
+    readonly defaultBalanceAccountName: string   // do all accounts have a default balance account?
+  ) {
     this.rules = new Map();
     mappings.forEach((names: string[], account: string) => {
       // TODO: detect if the same name is assigned to multiple accounts
@@ -22,9 +22,9 @@ export class PostingRules {
 
 
 
-      //   // maybe CSVRecord is not necessary, can just use naming & posting converters on the CSV output
-      //   (amountCents < 0) ? account.defaultSrcAccount : account.defaultDestAccount,
-      //   (amountCents < 0) ? account.defaultDestAccount : account.defaultSrcAccount
+  //   // maybe CSVRecord is not necessary, can just use naming & posting converters on the CSV output
+  //   (amountCents < 0) ? account.defaultSrcAccount : account.defaultDestAccount,
+  //   (amountCents < 0) ? account.defaultDestAccount : account.defaultSrcAccount
 
   //
   // +/-  | type   || src       | desc
@@ -33,7 +33,7 @@ export class PostingRules {
   // +    | credit || income:*  | default
   // -    | income || default   | assets:*
   // +    | income || what does this mean? returning salary?
-  
+
   // 2021-05-15 Stuff  ; id:2020052600001
   //   expenses:food                  $4.55
   //   liabilities:visa              $-4.55
@@ -42,7 +42,7 @@ export class PostingRules {
 
   // 2021-10-10 paycheck
   //   income:salary      -$5.00
-  //   assets:checking     $5.00 
+  //   assets:checking     $5.00
 
   // 2021-10-10 lunch
   //   expenses:food        $5.00
@@ -54,9 +54,12 @@ export class PostingRules {
   apply(canonicalName: string, amountCents: number): Posting[] {
     const account = this.rules.get(canonicalName);
     if (account) {
-      return Just(account);
+      // TODO: which posting goes first by default?
+      return [new Posting(0, this.accountName, amountCents), new Posting(1, account, amountCents * -1)];
+    } else if (!account && amountCents < 0) {
+      return [new Posting(0, this.accountName, amountCents), new Posting(1, "expenses:unclassified", amountCents * -1)];
     } else {
-      return Nothing();
+      return new Array();
     }
   }
 }
---
+++ tests/postingRules.test.ts
@@ -0,0 +1,30 @@
+import { AccountType } from '../src/server/account';
+import { Posting } from '../src/server/posting';
+import { PostingRules } from '../src/server/postingRules';
+
+test("handles an expense that is mapped successfully", () => {
+  const mappings = new Map<string, Array<string>>();
+  const accountName = "liabilities:credit card:amex";
+  mappings.set("expenses:food:groceries", ["Food World"]);
+  const rules = new PostingRules(mappings, accountName, AccountType.Credit, "assets:checking:credit union");
+
+  expect(rules.apply("Food World", -123)).toEqual([new Posting(0, accountName, -123), new Posting(1, "expenses:food:groceries", 123)]);
+});
+
+test("handles an expense that is not mapped successfully", () => {
+  const mappings = new Map<string, Array<string>>();
+  const accountName = "liabilities:credit card:amex";
+  mappings.set("expenses:food:groceries", ["Food World"]);
+  const rules = new PostingRules(mappings, accountName, AccountType.Credit, "assets:checking:credit union");
+
+  expect(rules.apply("GRCRY STRE 123", -456)).toEqual([new Posting(0, accountName, -456), new Posting(1, "expenses:unclassified", 456)]);
+});
+
+test("handles income that is mapped successfully", () => {
+  const mappings = new Map<string, Array<string>>();
+  const accountName = "liabilities:credit card:amex";
+  mappings.set("expenses:food:groceries", ["Food World"]);
+  const rules = new PostingRules(mappings, accountName, AccountType.Credit, "assets:checking:credit union");
+
+  expect(rules.apply("GRCRY STRE 123", -456)).toEqual([new Posting(0, accountName, -456), new Posting(1, "expenses:unclassified", 456)]);
+});
